f51af58c9b11bbcc6836ebf26ab6dc52f2e340c2
mgmtsweni <mtswenigift@gmail.com>
2022-09-24 12:00:33 +0200

    update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,3 @@
 #!/usr/bin/env ruby
-print ARGV[0].scan(/\[from:.+?\]/).join.sub!(/\[from:/, "").sub!(/\]/, "")
-print ","
-print ARGV[0].scan(/\[to:.+?\]/).join.sub!(/\[to:/, "").sub!(/\]/, "")
-print ","
-puts ARGV[0].scan(/\[flags:.+?\]/).join.sub!(/\[flags:/, "").sub!(/\]/, "")
+
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
