544c58eb51e3b5c811143b3cc6460a81f991363c
mgmtsweni <mtswenigift@gmail.com>
2022-10-15 05:16:42 +0200

    Create

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creating a file in /tmp
+
+file {
+    path    =>  '/tmp/school'
+    mode    =>  '0744'
+    owner   =>  'www-data'
+    group   =>  'www-data'
+    content =>  'I love Puppet'
+}
