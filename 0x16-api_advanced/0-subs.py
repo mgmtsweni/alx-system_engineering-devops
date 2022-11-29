9f4fab37cad32bddbf2388c5c5d32ec447485662
mgmtsweni <mtswenigift@gmail.com>
2022-11-30 01:54:51 +0200

    Code Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,16 @@ from requests import get
 def number_of_subscribers(subreddit):
     """ Returns subscriber count of subreddit or 0 """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = {'user-agent': 'my-app/0.0.1'}
+    headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
 
     r = get(url, headers=headers, allow_redirects=False)
 
