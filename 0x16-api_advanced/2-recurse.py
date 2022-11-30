0025cc85730a15c0cc1fe715ae8732b162dbab5b
mgmtsweni <mtswenigift@gmail.com>
2022-11-30 02:38:44 +0200

    Code correct

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 from requests import get
 
 
-def recurse(subreddit, hot_list=[], after= None):
+def recurse(subreddit, hot_list=[], after=None):
     """
     recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit
@@ -26,26 +26,15 @@ def recurse(subreddit, hot_list=[], after= None):
     }
 
     re = get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if re.status_code != 200:
-        return None
 
-    try:
+    if re.status_code == 200:
         js = re.json()
-
-    except ValueError:
-        return None
-
-    try:
-
         data = js.get("data")
         after = data.get("after")
         children = data.get("children")
         for child in children:
             post = child.get("data")
             hot_list.append(post.get("title"))
-
-    except:
+        return recurse(subreddit, hot_list, after)
+    else:
         return None
-
-    return recurse(subreddit, hot_list, after)
