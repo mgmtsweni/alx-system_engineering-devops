f1f1685b65ac1b7c3a53b2a242893399fa7f14b8
mgmtsweni <mtswenigift@gmail.com>
2022-12-11 10:49:51 +0200

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Fix Nginx
-exec {'improve nginx perfomance':
+exec {'improve-nginx-perfomance':
   command =>  'sudo sed -i "s/15/5000/" /etc/default/nginx',
   path    =>  '/usr/local/bin/:/bin/'
 } ->
 
-exec {'Restart Nginx':
+exec {'Restart-Nginx':
   command =>  'sudo  service nginx restart',
-  path    =>  '/etc/init.d'
+  path    =>  '/usr/bin/env'
 }
